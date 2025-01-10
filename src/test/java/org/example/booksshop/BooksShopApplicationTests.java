cd877eccbc96dd233f327e9b1cfa7d65d697f27a
Student <erkany28.ey@gmail.com>
2025-01-10 14:19:53 +0100

    initial commit

---
+++ src/test/java/org/example/booksshop/BooksShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.booksshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
