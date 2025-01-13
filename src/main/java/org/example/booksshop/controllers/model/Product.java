778793348e1d83e758456e82eb40dfc6e7d2fec5
Student <erkany28.ey@gmail.com>
2025-01-13 12:01:01 +0100

    initial commit

---
+++ src/main/java/org/example/booksshop/controllers/model/Product.java
@@ -0,0 +1,9 @@
+package org.example.booksshop.controllers.model;
+
+public class Product {
+    protected long id;
+    protected String name;
+    protected double price;
+    protected String description;
+    protected String imageURL;
+}
